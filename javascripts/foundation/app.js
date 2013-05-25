578aaba25dce6046354a014faa2ec16d7b7af60a
Laura Cabrera <lzcabrera@gmail.com>
2013-05-24 17:48:12 -0700

    add hello lucas blog post

--- javascripts/foundation/app.js
+++ javascripts/foundation/app.js
@@ -31,15 +31,77 @@
     });
   }
 
-  $.ajax({
+ /* $.ajax({
     type: 'GET',
-    url: 'https://api.twitter.com/1/statuses/oembed.json?id=251508898299777026',
+    url: 'https://api.twitter.com/1/statuses/user_timeline.json?screen_name=lauracabrera&count=1',
     dataType: 'jsonp',
     success: function(json) {
-        var result = json.html;
-        $('#twitter-tweet-lc').append(result);
+       // var result = json.html;
+        //alert(json[0]);
+        //$('#twitter-tweet-lc').append(result);
+
+       
+
+       
+
+
     }
+  });*/
+
+  // UPDATE 10-17-2012: change in Twitter API!
+/*$.getJSON("https://api.twitter.com/1/statuses/user_timeline.json?screen_name=lauracabrera&count=5&callback=?",
+ function(data){
+  $.each(data, function(i,item){
+   ct = item.text;
+  // include time tweeted - thanks to will
+  mytime = item.created_at;
+  var strtime = mytime.replace(/(\+\S+) (.*)/, '$2 $1')
+  var mydate = new Date(Date.parse(strtime)).toLocaleDateString();
+  var mytime = new Date(Date.parse(strtime)).toLocaleTimeString();
+   ct = ct.replace(/http:\/\/\S+/g,  '<a href="$&" target="_blank">$&</a>');
+   ct = ct.replace(/\s(@)(\w+)/g,    ' @<a onclick="javascript:pageTracker._trackPageview('/outgoing/twitter.com/');" href="http://twitter.com/$2" target="_blank">$2</a>');
+   ct = ct.replace(/\s(#)(\w+)/g,    ' #<a onclick="javascript:pageTracker._trackPageview('/outgoing/search.twitter.com/search?q=%23');" href="http://search.twitter.com/search?q=%23$2" target="_blank">$2</a>');
+   
+  $("#twitter-tweet-lc").append('
+<div>'+ct + " <small><i>(" + mydate + " @ " + mytime + ")</i></small></div>
+
+");
   });
+ });*/
+
+
+
+
+/*
+$.getJSON("https://api.twitter.com/1/statuses/user_timeline/uniofgreenwich.json?count=1&callback=?",
+    function(data){
+      $("#twitter-tweet-lc").append("<a href='http://twitter.com/#!/uniofgreenwich/status/"+data[0].id_str+"'>" + data[0].text + "</a>");
+  });
+
+*/
+
+/*
+  <ul id="tweetList"></ul>
+    <script type="text/javascript" charset="utf-8">
+      var twitterCallback = function(tweets) {
+        var i, tweetList = $("#twitter-tweet-lc");
+        for( i = 0; i < tweets.length; i++ ) {
+          //tweetList.innerHTML = tweetList.innerHTML + '<li>' + tweets[i].text + '</li>';
+          tweetList.append('<li>' + tweets[i].text + '</li>');
+        }
+      };
+    </script>
+    <script src="http://api.twitter.com/1/statuses/user_timeline.json?screen_name=lauracabrera&count=3&callback=twitterCallback" 
+              type="text/javascript" 
+              charset="utf-8">
+    </script>
+
+
+ $.ajax({
+  url:'https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=twitterapi&count=2',
+  dataType:'json',
+  success:function(data){ console.log(data); }
+});*/
 
   $("#sample_user_badge").GitHubBadge({
                 login: "lzcabrera",
